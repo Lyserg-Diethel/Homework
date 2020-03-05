2832f76a96cee610e2476414987721acdf8310a9
Lyserg <Lyserg-Diethel@protonmail.com>
2020-03-05 22:24:48 +0200

    Added function to compare rectangle areas.

---
+++ maxRectangleArea.js
@@ -0,0 +1,65 @@
+/*
+Represent the data about 5 rectangles wih the following properties:
+
+	id:'r1' , height: 40 , width: 20,
+	id:'r2' , height: 30, width: 30,
+	id:'r3' , height: 10, width: 80,
+	id:'r4' , height: 90, width: 2,
+	id:'r5' , height: 20, width: 20,
+
+*/
+
+
+let rectangles = {
+	'r1':{
+		'id': 'r1',
+		'height': 40, 
+		'width': 20,
+	},
+	'r2':{
+		'id': 'r2',
+		'height': 30, 
+		'width': 30,
+	},
+	'r3':{
+		'id': 'r3',
+		'height': 10, 
+		'width': 80,
+	},
+	'r4':{
+		'id': 'r4',
+		'height': 90, 
+		'width': 2,
+	},
+	'r5':{
+		'id': 'r5',
+		'height': 20, 
+		'width': 20,
+	},
+
+}
+
+function calcArea(rect){
+	return (this['height'] * this['width']);
+}
+
+let findLargestRectangle = function(rectangles){
+    let largestRectID;
+    let max = 0;
+    let compare;
+
+    for(each in rectangles){
+        compare = (rectangles[`${each}`].height) * (rectangles[`${each}`].width);
+
+        if(compare > max) {
+            max = compare;
+            largestRectID = rectangles[`${each}`].id;
+        };
+    }
+    return largestRectID;
+}
+
+
+
+//Write a function which will return the id of the rectangle with the bigest area.
+//Exepcted output: r2.
